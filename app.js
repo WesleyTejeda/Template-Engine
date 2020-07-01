ef90bcc597620141defa3b365601ca9d97140b0a
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-01 14:53:28 -0400

    Added write function to generate html file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 let employeeArr = [];
-function addMember(){
+function Init(){
     inquirer.prompt([
         {
             type: "rawlist",
@@ -44,7 +44,7 @@ function addMember(){
                 obj.officeNumber = placeHolder[3];
                 const person = new Manager(obj.name, obj.id, obj.email, obj.officeNumber);
                 employeeArr.push(person);
-                addMember();
+                Init();
             })
             
         }
@@ -64,7 +64,7 @@ function addMember(){
                 obj.github = placeHolder[3];
                 const person = new Engineer(obj.name, obj.id, obj.email, obj.github);
                 employeeArr.push(person);
-                addMember();
+                Init();
             })
         }
         if (resp.role === "Intern"){
@@ -83,18 +83,16 @@ function addMember(){
                 obj.school = placeHolder[3];
                 const person = new Intern(obj.name, obj.id, obj.email, obj.school);
                 employeeArr.push(person);
-                addMember();
+                Init();
             })
         }
         if(resp.role === "Exit"){
             console.log("Exiting input. Generating HTML...");
             console.log(employeeArr);
-            // return render(employeeArr);
 
-            let html = render(employeeArr);
-            console.log(html);
-            console.log("HTML generated!");
-            return null;
+            console.log("Rendering!");
+            // return render(employeeArr);
+            generateHTML();
         }
     });
 }
@@ -102,19 +100,27 @@ function addMember(){
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// addMember();
-// let html = addMember();
-// console.log(html);
-// console.log("Rendering");
-// console.log(html);
-// console.log("HTML generated!");
+//
+Init();
+function generateHTML(){
+    let html = render(employeeArr);
+    console.log(html);
+    console.log("HTML generated!");
+    writeToFile(html);
+}
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
+function writeToFile(html){
+    fs.writeFile("team.html", html, (err) => {
+        if (err)
+            throw err;
+        else return "Successfully created and wrote to HTML file";
+    })
+}
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
